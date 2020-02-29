a3577e9a0a448ec9bcd0a52fb0c171f530df472d
SArtur <artewal@gmail.com>
2020-02-29 16:29:30 +0100

    adding tests

---
+++ src/eclipsetest/BackgroundClassTest.java
@@ -0,0 +1,24 @@
+package eclipsetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BackgroundClassTest {
+
+	@Test
+	void testStartBGThread() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCoronaInfection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConflictFunction() {
+		fail("Not yet implemented");
+	}
+
+}
