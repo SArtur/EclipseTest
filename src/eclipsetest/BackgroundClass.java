bbeac2f2f92cdb0ace6395b7a35bcb1d2cf587f8
SArtur <artewal@gmail.com>
2020-02-29 11:21:54 +0100

    not much added

--- src/eclipsetest/BackgroundClass.java
+++ src/eclipsetest/BackgroundClass.java
@@ -5,4 +5,12 @@ public class BackgroundClass {
 	public void startBGThread() {
 Thread thread = new Thread (() -> {System.out.println("yo");});
 	}
+	
+	public void coronaInfection (boolean infected) {
+		if (infected) {
+			Thread thread = new Thread(() -> {System.out.println("infection has started.");});
+		} else {
+			Thread thread = new Thread(() -> {System.out.println("No treatment needed.");});
+		}
+	}
 }
